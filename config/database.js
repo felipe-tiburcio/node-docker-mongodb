e655cf76b1180a16c454443ec9981b308bf8aaa1
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-12-16 14:59:23 -0300

    refactor: change in mongodb url connection

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const URL = "mongodb://localhost:27017/mydatabase";
+const URL = "mongodb://root:password@localhost:27017/mydatabase?authSource=admin";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(URL);
     console.log("MongoDB connected!");
   } catch (error) {
-    console.error(`Database connection error: ${error}`);
+    console.error(`MongoDB connection error: ${error}`);
     process.exit(1);
   }
 };
